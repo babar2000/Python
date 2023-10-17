5b555a28b5318e5d260e5b348cfc5a52c0f7c80e
Zain Babar <zainibaba1990@yahoo.com>
2023-10-17 18:56:00 +0500

    update python with installation and data types

--- 01_class(Hello world_string)/class1.py
+++ 01_class(Hello world_string)/class1.py
@@ -1,2 +1,5 @@
-print("Pakistan")
-print("Zindabad")
+name : str= "Zain Babar"
+print(name) #print name
+print(type(name)) # type
+print(id(name)) # physical address
+print([i for i in dir(name) if "_" not in i]) # methods and attributes 
