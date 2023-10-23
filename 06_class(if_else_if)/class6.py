1baf66947a76253942f1e93a74ab8b96b16e5294
Zain Babar <zainibaba1990@yahoo.com>
2023-10-23 05:24:07 +0500

    if_elif_else

---
+++ 06_class(if_else_if)/class6.py
@@ -0,0 +1,22 @@
+# union type (assign multiple types)
+from typing import Union
+
+percentage : Union[int,float] = 88
+grade : Union[str,None] = 7 
+
+if percentage >= 80:
+    grade = "A+"
+elif percentage >= 70:
+    grade = "A"
+elif percentage >= 60:
+    grade = "B"
+elif percentage >= 50:
+    grade = "C"
+elif percentage >= 40:
+    grade = "D"
+elif percentage >= 33:
+    grade = "E"
+else:
+    grade = "Fail"
+print(f"Dear Student your peercentage is {percentage} now your calculated grade is: \t{grade}")
+
