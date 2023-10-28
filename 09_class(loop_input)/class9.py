8519bc2000460f99123e1846a4c57fa97abeb520
Zain Babar <zainibaba1990@yahoo.com>
2023-10-28 14:49:46 +0500

    loop_input

---
+++ 09_class(loop_input)/class9.py
@@ -0,0 +1,9 @@
+# name : str = input("What is your name?: \t")
+# print(type(name))
+# print(f"Welcome dear user Mr/Miss {name}!")
+import sys # package name
+
+print("line1")
+print("line2")
+
+print(sys.argv) # iterative data type argv-argument value
